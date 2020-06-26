5f798ac61fc9c0dc3a7d59d6453d69f1a3ef8476
Sergey Eliseev <thefury014@gmail.com>
2020-06-26 16:52:41 +0900

    Fix vue-property-decorator extend

--- types/vue-extend.ts
+++ types/vue-extend.ts
@@ -1,9 +1,15 @@
-import { Vue } from 'vue-property-decorator';
-import NativeScriptVue, { navigateTo, ModalOptions } from 'nativescript-vue';
+import 'vue-property-decorator';
+import { navigateTo, ModalOptions } from 'nativescript-vue';
 import { View } from 'tns-core-modules/ui/core/view';
 
 declare module 'vue-property-decorator' {
-  interface Vue<V = View> extends NativeScriptVue<V> {
+  /**
+   * All the properties are picked from
+   * NativeScriptVue interface from nativescript-vue
+   * module because vue-property-decorator doesn't
+   * know about nativescript-vue properties
+   */
+  interface Vue<V = View> {
     nativeView: V
 
     $navigateTo: navigateTo
